480e6b0c0f1491f8887cc2a0487c4b58d2a8c966
AdanStudent <adan.benitez@loop.colum.edu>
2018-09-23 14:08:14 -0500

    Finished Homework

---
+++ ab-TranslatedTriangle/TranslatedTriangle.js
@@ -0,0 +1,91 @@
+﻿//Translated Triangle.js
+//Vertexshader program
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'uniform vec4 u_Translation;\n' +
+    'void main() {\n' +
+    ' gl_Position = a_Position + u_Translation;\n' +
+    '}\n';
+
+// Fragment Shader program
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+    '}\n';
+
+//the translation distance for x, y and z direction
+var Tx = 0.5, Ty = 0.5, Tz = 0.0;
+
+function main() {
+    //Retreive <canvas>
+    var canvas = document.getElementById('webgl');
+
+    //get rendering context
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    //Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initalize shaders');
+        return;
+    }
+
+    //write the positions of vertices to a vertex shader
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('failed to set the positions of the vertices');
+        return;
+    }
+
+    //pass the translation disctance to the vertex shader
+    var u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+    if (!u_Translation) {
+        console.log('failed to get u_Translation');
+        return;
+    }
+    gl.uniform4f(u_Translation, Tx, Ty, Tz, 0.0);
+          
+    //specify the color for clearing <canvas>
+    gl.clearColor(0, 0, 0, 1);
+
+    //Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //draw the rectangle
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}//end of main
+
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+
+    var n = 3;
+
+    //create Buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('failed to create the buffer object');
+        return -1;
+    }
+
+    //bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    //write data into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('failed to get the storage location of a_Position');
+        return -1;
+    }
+    //assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    //enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
